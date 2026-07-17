28351f77c2742d8e796489193bcf5f606190d557
唐召明 <2270969436@qq.com>
2026-07-17 15:39:10 +0800

    完善日志组件

--- Admin.Desktop/UserControls/LogView.xaml.cs
+++ Admin.Desktop/UserControls/LogView.xaml.cs
@@ -12,82 +12,116 @@ namespace Admin.Desktop.UserControls
     /// </summary>
     public partial class LogView : UserControl
     {
-        private readonly Lock _writeLocker = new Lock();
+        private readonly Lock _appendLog = new Lock();
         public LogView()
         {
             InitializeComponent();
         }
 
-        public int MaxLineCount
+        public int MaxLogCount
         {
-            get => (int)GetValue(MaxLineCountProperty);
-            set => SetValue(MaxLineCountProperty, value);
+            get => (int)GetValue(MaxLogCountProperty);
+            set => SetValue(MaxLogCountProperty, value);
         }
 
-        public static readonly DependencyProperty MaxLineCountProperty =
+        public static readonly DependencyProperty MaxLogCountProperty =
             DependencyProperty.Register(
-                nameof(MaxLineCount),
+                nameof(MaxLogCount),
                 typeof(int),
                 typeof(LogView),
                 new PropertyMetadata(100)); // 默认100行
 
         /// <summary>
-        /// 通用输出日志
+        /// Gets or sets a value indicating whether to add new event at the end.
+        /// </summary>
+        public bool AddAtTail
+        {
+            get { return (bool)this.GetValue(AddAtTailProperty); }
+            set { this.SetValue(AddAtTailProperty, value); }
+        }
+
+        /// <summary>
+        /// Dependency property for <c>AddAtTail</c>.
+        /// </summary>
+        public static readonly DependencyProperty AddAtTailProperty =
+          DependencyProperty.Register(
+              nameof(AddAtTail),
+              typeof(bool),
+              typeof(LogView),
+              new PropertyMetadata(true));
+
+        /// <summary>
+        /// 通用日志输出
         /// </summary>
         /// <param name="msg"></param>
         /// <param name="dateTime"></param>
         /// <param name="color"></param>
-        private void Write(string msg, DateTime? dateTime = null, SolidColorBrush? color = null)
+        private void AppendLog(string msg, DateTime? dateTime = null, SolidColorBrush? color = null)
         {
-            lock (_writeLocker)
+            Dispatcher.Invoke(() =>
             {
-                // 记录当前滚动位置
-                var currentOffset = rtbLog.VerticalOffset;
-                // 超出最大行删除最旧一条
-                while (rtbLog.Document.Blocks.Count > MaxLineCount)
+                lock (_appendLog)
                 {
-                    rtbLog.Document.Blocks.Remove(rtbLog.Document.Blocks.FirstBlock);
+                    // 记录当前滚动位置
+                    var currentOffset = RtbLog.VerticalOffset;
+
+                    var blocks = RtbLog.Document.Blocks;
+                    // 超出最大行删除最旧一条
+                    while (blocks.Count > MaxLogCount)
+                    {
+                        var waitRemoveBlock = AddAtTail ? blocks.FirstBlock : blocks.LastBlock;
+                        blocks.Remove(waitRemoveBlock);
+                    }
+
+                    var para = new Paragraph
+                    {
+                        Margin = new Thickness(0),
+                        Padding = new Thickness(0, 0.5, 0, 0.5)
+                    };
+
+                    // 时间
+                    para.Inlines.Add(new Run($"[{dateTime ?? DateTime.Now}] ")
+                    {
+                        Foreground = GetHcBrush(ResourceToken.SecondaryTextBrush)
+                        //Foreground = Foreground = Brushes.Gray
+                    });
+
+                    var contentRun = new Run(msg);
+                    if (color != null)
+                    {
+                        contentRun.Foreground = color;
+                    }
+                    // 日志内容 
+                    para.Inlines.Add(contentRun);
+                    if (AddAtTail)
+                    {
+                        blocks.Add(para);
+                    }
+                    else
+                    {
+                        blocks.InsertBefore(blocks.FirstBlock, para);
+                    }
+
+                    //滚动条位置保持不变
+                    RtbLog.ScrollToVerticalOffset(currentOffset);
                 }
-
-                var para = new Paragraph
-                {
-                    Margin = new Thickness(0, 1, 0, 1),
-                    Padding = new Thickness(0)
-                };
-
-                // 时间
-                para.Inlines.Add(new Run($"[{dateTime ?? DateTime.Now}] ")
-                {
-                    Foreground = GetHcBrush(ResourceToken.SecondaryTextBrush)
-                    //Foreground = Foreground = Brushes.Gray
-                });
-
-                var contentRun = new Run(msg);
-                if (color != null)
-                {
-                    contentRun.Foreground = color;
-                }
-                // 日志内容 
-                para.Inlines.Add(contentRun);
-                rtbLog.Document.Blocks.Add(para);
-                rtbLog.ScrollToVerticalOffset(currentOffset);
-            }
+            });
         }
 
-        public void Info(string msg, DateTime? dateTime = null) => Write(msg, dateTime, null);
+        public void Info(string msg, DateTime? dateTime = null) => AppendLog(msg, dateTime, null);
 
-        public void Success(string msg, DateTime? dateTime = null) => Write(msg, dateTime, GetHcBrush(ResourceToken.DarkSuccessBrush));
+        public void Success(string msg, DateTime? dateTime = null) => AppendLog(msg, dateTime, GetHcBrush(ResourceToken.DarkSuccessBrush));
 
-        public void Warn(string msg, DateTime? dateTime = null) => Write(msg, dateTime, GetHcBrush(ResourceToken.DarkWarningBrush));
+        public void Warn(string msg, DateTime? dateTime = null) => AppendLog(msg, dateTime, GetHcBrush(ResourceToken.DarkWarningBrush));
 
-        public void Error(string msg, DateTime? dateTime = null) => Write(msg, dateTime, GetHcBrush(ResourceToken.DarkDangerBrush));
+        public void Error(string msg, DateTime? dateTime = null) => AppendLog(msg, dateTime, GetHcBrush(ResourceToken.DarkDangerBrush));
 
         /// <summary>
         /// 清空全部日志
         /// </summary>
         public void Clear()
         {
-            rtbLog.Document.Blocks.Clear();
+            RtbLog.Document.Blocks.Clear();
         }
 
         private static SolidColorBrush? GetHcBrush(string resourceToken)
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# WpfAdmin
+# Admin Sample
 
-简体中文说明文档（简要）
+简要说明
 
 ## 项目简介
-`WpfAdmin` 是一个基于ABP vNext的WPF桌面管理后台示例/模版，包含典型的 MVVM 结构，用于演示桌面端管理系统的架构与实现思路。
+`Admin Sample` 是一个基于ABP vNext的通用权限管理框架，包含WPF与Web项目示例。
 
 ## 主要特性
 - 基于 WPF 的桌面 UI（MVVM 模式）
@@ -12,16 +12,17 @@
 
 ## 目录结构
 - `Admin.Desktop` — WPF 客户端
-- `Admin.HttpApi.Host` — 接口服务端
+- `Admin.HttpApi.Host` — Api 接口服务
+- `Admin.DbMigrator` — 数据迁移控制台程序
+- `Admin.Web` — 基于Layui 的 Web 项目 
 
 ## 贡献
 欢迎提交 Issue / Pull Request。请在提交前：
 - 保持代码风格一致
 - 添加必要的注释与单元测试（如适用）
 
-
 ## 许可证
-Apache-2.0
+[Apache-2.0](./LICENSE.txt)
 
 ## 联系
 如需帮助或讨论，请在仓库Issue 中创建条目。
